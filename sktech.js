3ce429639c31dfda22b110e16e4d740b624d867c
Pedro Vecchio Vieira <148560033+pedringamesplay@users.noreply.github.com>
2023-10-27 12:12:26 -0300

    adicionando arquivos js e css

---
+++ sktech.js
@@ -0,0 +1,15 @@
+function setup() {
+  
+    createCanvas(900,900);
+  
+  background("white");
+  
+  }
+    
+  function draw() {
+  
+    stroke ("blue");
+  
+    fill("red");
+    rect(mouseX, mouseY, 50, 50);
+     }
